a952635d6dda62598d009ec9a0d3d4b4f314ae38
GameDog9988 <alex@grattan.me>
2022-03-11 11:01:02 -0500

    fix: better background gradient for dashboard

--- screens/Dashboard.tsx
+++ screens/Dashboard.tsx
@@ -30,193 +30,202 @@ const DashboardHome = ({ navigation, route }) => {
   const [password, setPassword] = useState("");
 
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView contentContainerStyle={styles.scrollView}>
+    <>
+      <SafeAreaView style={{ backgroundColor: "#014E6F" }} />
+      <SafeAreaView style={styles.container}>
         <LinearGradient
           // Background Linear Gradient
-          colors={["#014E6F", "#223054"]}
+          colors={["#014E6F", "#014E6F", "#223054", "#223054"]}
           style={styles.gradientBackground}
+          locations={[0, 0.5, 0.6, 1.0]}
         />
-        <Text style={styles.title}>Select a Mode</Text>
-        <TouchableOpacity
-          style={[styles.modeButton, { backgroundColor: "#A038B1" }]}
-          onPress={() => {
-            route.params.rootNavigation.navigate("Lesson", {
-              mode: "Fact or Opinion",
-            });
-          }}
-        >
-          <Text style={styles.modeButtonText}>Fact</Text>
-          <View
-            style={{
-              width: 32,
-              height: 32,
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: 20,
-              backgroundColor: colors.secondary.purple,
-              marginHorizontal: 8,
+        <ScrollView contentContainerStyle={styles.scrollView}>
+          <LinearGradient
+            // Background Linear Gradient
+            colors={["#014E6F", "#223054"]}
+            style={styles.gradientBackground}
+          />
+          <Text style={styles.title}>Select a Mode</Text>
+          <TouchableOpacity
+            style={[styles.modeButton, { backgroundColor: "#A038B1" }]}
+            onPress={() => {
+              route.params.rootNavigation.navigate("Lesson", {
+                mode: "Fact or Opinion",
+              });
             }}
           >
-            <Text
+            <Text style={styles.modeButtonText}>Fact</Text>
+            <View
               style={{
-                fontSize: 12,
-                color: "white",
+                width: 32,
+                height: 32,
                 justifyContent: "center",
                 alignItems: "center",
-                textTransform: "uppercase",
+                borderRadius: 20,
+                backgroundColor: colors.secondary.purple,
+                marginHorizontal: 8,
               }}
             >
-              Or
-            </Text>
-          </View>
-          <Text style={styles.modeButtonText}>Opinion</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[
-            styles.modeButton,
-            { backgroundColor: "#2068B7", marginTop: 8 },
-          ]}
-          onPress={() => {
-            route.params.rootNavigation.navigate("Lesson", {
-              mode: "Evidence or Not",
-            });
-          }}
-        >
-          <Text style={styles.modeButtonText}>Evidence</Text>
-          <View
-            style={{
-              width: 32,
-              height: 32,
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: 20,
-              backgroundColor: colors.secondary.purple,
-              marginHorizontal: 8,
+              <Text
+                style={{
+                  fontSize: 12,
+                  color: "white",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  textTransform: "uppercase",
+                }}
+              >
+                Or
+              </Text>
+            </View>
+            <Text style={styles.modeButtonText}>Opinion</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[
+              styles.modeButton,
+              { backgroundColor: "#2068B7", marginTop: 8 },
+            ]}
+            onPress={() => {
+              route.params.rootNavigation.navigate("Lesson", {
+                mode: "Evidence or Not",
+              });
             }}
           >
-            <Text
+            <Text style={styles.modeButtonText}>Evidence</Text>
+            <View
               style={{
-                fontSize: 12,
-                color: "white",
+                width: 32,
+                height: 32,
                 justifyContent: "center",
                 alignItems: "center",
-                textTransform: "uppercase",
+                borderRadius: 20,
+                backgroundColor: colors.secondary.purple,
+                marginHorizontal: 8,
               }}
             >
-              Or
-            </Text>
-          </View>
-          <Text style={styles.modeButtonText}>Not</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[
-            styles.modeButton,
-            { backgroundColor: "#35C3E9", marginTop: 8 },
-          ]}
-          onPress={() => {
-            route.params.rootNavigation.navigate("Lesson", {
-              mode: "Ad or Not",
-            });
-          }}
-        >
-          <Text style={styles.modeButtonText}>Ad</Text>
-          <View
-            style={{
-              width: 32,
-              height: 32,
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: 20,
-              backgroundColor: colors.secondary.purple,
-              marginHorizontal: 8,
+              <Text
+                style={{
+                  fontSize: 12,
+                  color: "white",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  textTransform: "uppercase",
+                }}
+              >
+                Or
+              </Text>
+            </View>
+            <Text style={styles.modeButtonText}>Not</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[
+              styles.modeButton,
+              { backgroundColor: "#35C3E9", marginTop: 8 },
+            ]}
+            onPress={() => {
+              route.params.rootNavigation.navigate("Lesson", {
+                mode: "Ad or Not",
+              });
             }}
           >
-            <Text
+            <Text style={styles.modeButtonText}>Ad</Text>
+            <View
               style={{
-                fontSize: 12,
-                color: "white",
+                width: 32,
+                height: 32,
                 justifyContent: "center",
                 alignItems: "center",
-                textTransform: "uppercase",
+                borderRadius: 20,
+                backgroundColor: colors.secondary.purple,
+                marginHorizontal: 8,
               }}
             >
-              Or
-            </Text>
-          </View>
-          <Text style={styles.modeButtonText}>Not</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[
-            styles.modeButton,
-            { backgroundColor: "#6932EB", marginTop: 8 },
-          ]}
-          onPress={() => {
-            route.params.rootNavigation.navigate("Lesson", {
-              mode: "News or Opinion",
-            });
-          }}
-        >
-          <Text style={styles.modeButtonText}>News</Text>
-          <View
-            style={{
-              width: 32,
-              height: 32,
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: 20,
-              backgroundColor: colors.secondary.purple,
-              marginHorizontal: 8,
+              <Text
+                style={{
+                  fontSize: 12,
+                  color: "white",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  textTransform: "uppercase",
+                }}
+              >
+                Or
+              </Text>
+            </View>
+            <Text style={styles.modeButtonText}>Not</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[
+              styles.modeButton,
+              { backgroundColor: "#6932EB", marginTop: 8 },
+            ]}
+            onPress={() => {
+              route.params.rootNavigation.navigate("Lesson", {
+                mode: "News or Opinion",
+              });
             }}
           >
-            <Text
+            <Text style={styles.modeButtonText}>News</Text>
+            <View
               style={{
-                fontSize: 12,
-                color: "white",
+                width: 32,
+                height: 32,
                 justifyContent: "center",
                 alignItems: "center",
-                textTransform: "uppercase",
+                borderRadius: 20,
+                backgroundColor: colors.secondary.purple,
+                marginHorizontal: 8,
               }}
             >
-              Or
-            </Text>
-          </View>
-          <Text style={styles.modeButtonText}>Opinion</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.mixUpMode, { marginTop: 8 }]}>
-          <ImageBackground source={MixUp} style={styles.mixModeImage}>
-            <FontAwesome
-              name="lock"
-              size={32}
-              color="#164F66"
-              style={{ marginTop: -32, marginBottom: 4 }}
-            />
-            <Text style={styles.mixUpModeText}>Mix-Up {"\n"} Mode</Text>
-            <Text
-              style={{
-                position: "absolute",
-                bottom: 8,
-                color: "white",
-                fontSize: 12,
-                backgroundColor: "rgba(0, 0, 0, 0.25)",
-                padding: 4,
-                borderRadius: 4,
-              }}
-            >
-              Complete the 4 challenges above to unlock
-            </Text>
-          </ImageBackground>
-        </TouchableOpacity>
-        <OpacityButton
-          buttonType="custom"
-          buttonStyles={styles.forgotPassword}
-          buttonText={styles.forgotPasswordText}
-        >
-          Learn more about the News Literacy Project
-        </OpacityButton>
-      </ScrollView>
-      <StatusBar style="light" />
-    </SafeAreaView>
+              <Text
+                style={{
+                  fontSize: 12,
+                  color: "white",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  textTransform: "uppercase",
+                }}
+              >
+                Or
+              </Text>
+            </View>
+            <Text style={styles.modeButtonText}>Opinion</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.mixUpMode, { marginTop: 8 }]}>
+            <ImageBackground source={MixUp} style={styles.mixModeImage}>
+              <FontAwesome
+                name="lock"
+                size={32}
+                color="#164F66"
+                style={{ marginTop: -32, marginBottom: 4 }}
+              />
+              <Text style={styles.mixUpModeText}>Mix-Up {"\n"} Mode</Text>
+              <Text
+                style={{
+                  position: "absolute",
+                  bottom: 8,
+                  color: "white",
+                  fontSize: 12,
+                  backgroundColor: "rgba(0, 0, 0, 0.25)",
+                  padding: 4,
+                  borderRadius: 4,
+                }}
+              >
+                Complete the 4 challenges above to unlock
+              </Text>
+            </ImageBackground>
+          </TouchableOpacity>
+          <OpacityButton
+            buttonType="custom"
+            buttonStyles={styles.forgotPassword}
+            buttonText={styles.forgotPasswordText}
+          >
+            Learn more about the News Literacy Project
+          </OpacityButton>
+        </ScrollView>
+        <StatusBar style="light" />
+      </SafeAreaView>
+    </>
   );
 };
 
